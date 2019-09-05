7fb3c5b8afa818f9c8031e61810312ed04a857c3
DmitriyLy <dmitriy.lisay@gmail.com>
2019-09-05 19:56:53 +0300

    1.84

--- traveller-persistence/src/main/java/org/dmly/traveller/app/persistence/repository/CityRepository.java
+++ traveller-persistence/src/main/java/org/dmly/traveller/app/persistence/repository/CityRepository.java
@@ -5,6 +5,7 @@ import org.dmly.traveller.app.model.entity.geography.City;
 import java.util.List;
 
 public interface CityRepository {
+    
     void save(City city);
     City findById(int cityId);
     void delete(int cityId);
