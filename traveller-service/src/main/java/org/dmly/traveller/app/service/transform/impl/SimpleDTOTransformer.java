f3743af439f743bcdc60f19b6c0d8984d3e56a5e
DmitriyLy <dmitriy.lisay@gmail.com>
2019-09-05 11:28:10 +0300

    1.61

--- traveller-service/src/main/java/org/dmly/traveller/app/service/transform/impl/SimpleDTOTransformer.java
+++ traveller-service/src/main/java/org/dmly/traveller/app/service/transform/impl/SimpleDTOTransformer.java
@@ -1,12 +1,17 @@
 package org.dmly.traveller.app.service.transform.impl;
 
 import org.dmly.traveller.app.infra.util.Checks;
+import org.dmly.traveller.app.infra.util.CommonUtil;
 import org.dmly.traveller.app.infra.util.ReflectionUtil;
 import org.dmly.traveller.app.model.entity.base.AbstractEntity;
 import org.dmly.traveller.app.rest.dto.base.BaseDTO;
 import org.dmly.traveller.app.service.transform.Transformer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SimpleDTOTransformer implements Transformer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleDTOTransformer.class);
+
     @Override
     public <T extends AbstractEntity, P extends BaseDTO<T>> P transform(final T entity, final Class<P> clz) {
         checkParams(entity, clz);
@@ -15,6 +20,9 @@ public class SimpleDTOTransformer implements Transformer {
         ReflectionUtil.copyFields(entity, dto, ReflectionUtil.findSimilarField(entity.getClass(), clz));
         dto.transform(entity);
 
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("SimpleDTOTransformer.transform: {} DTO object", CommonUtil.toString(dto));
+        }
 
         return dto;
     }
@@ -26,6 +34,10 @@ public class SimpleDTOTransformer implements Transformer {
         T entity = ReflectionUtil.createInstance(clz);
         ReflectionUtil.copyFields(dto, entity, ReflectionUtil.findSimilarField(dto.getClass(), clz));
         dto.untransform(entity);
+
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("SimpleDTOTransformer.untransform: {} entity", CommonUtil.toString(entity));
+        }
 
         return entity;
     }
