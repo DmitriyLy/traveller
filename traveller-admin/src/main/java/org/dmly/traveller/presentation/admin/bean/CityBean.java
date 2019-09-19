c15eb68b2c84116be8dbe8be3dd2837c6e228c6f
DmitriyLy <dmitriy.lisay@gmail.com>
2019-09-19 13:53:19 +0300

    1.158

--- traveller-admin/src/main/java/org/dmly/traveller/presentation/admin/bean/CityBean.java
+++ traveller-admin/src/main/java/org/dmly/traveller/presentation/admin/bean/CityBean.java
@@ -1,5 +1,8 @@
 package org.dmly.traveller.presentation.admin.bean;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.dmly.traveller.app.model.entity.geography.City;
 import org.dmly.traveller.app.model.transform.Transformable;
 
@@ -8,43 +11,13 @@ import javax.faces.bean.ViewScoped;
 
 @ManagedBean(name="currentCity")
 @ViewScoped
+@ToString
+@Getter @Setter
 public class CityBean implements Transformable<City> {
     private int id;
     private String name;
     private String district;
     private String region;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDistrict() {
-        return district;
-    }
-
-    public void setDistrict(String district) {
-        this.district = district;
-    }
-
-    public String getRegion() {
-        return region;
-    }
-
-    public void setRegion(String region) {
-        this.region = region;
-    }
 
     public void clear() {
         id = 0;
