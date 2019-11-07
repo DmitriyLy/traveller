6aef30ccba9fad4b1a07282c3548d318e20b233e
DmitriyLy <dmitriy.lisay@gmail.com>
2019-11-07 13:21:21 +0200

    2.60

--- traveller-web/client/src/app/app.module.ts
+++ traveller-web/client/src/app/app.module.ts
@@ -3,16 +3,35 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { CitiesComponent } from './cities/cities.component';
+import { TranslateModule, TranslateService, TranslateLoader } from '@ngx-translate/core';
+import { LanguageComponent } from './language/language.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CitiesComponent,
+    LanguageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
-  providers: [],
+  providers: [TranslateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
