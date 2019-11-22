aaecfeb2e1518406092a22e6fa971725bc60bcb9
DmitriyLy <dmitriy.lisay@gmail.com>
2019-11-22 18:48:04 +0200

    2.108

--- traveller-model/src/test/java/org/dmly/traveller/app/infra/util/ChecksTest.java
+++ traveller-model/src/test/java/org/dmly/traveller/app/infra/util/ChecksTest.java
@@ -1,9 +1,9 @@
 package org.dmly.traveller.app.infra.util;
 
 import org.dmly.traveller.app.infra.exception.flow.InvalidParameterException;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ChecksTest {
 
