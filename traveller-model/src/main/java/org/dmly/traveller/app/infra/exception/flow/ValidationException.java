f1759ccfe15ee968529929c1ced0640092389f8a
DmitriyLy <dmitriy.lisay@gmail.com>
2019-11-25 17:37:29 +0200

    2.127

--- traveller-model/src/main/java/org/dmly/traveller/app/infra/exception/flow/ValidationException.java
+++ traveller-model/src/main/java/org/dmly/traveller/app/infra/exception/flow/ValidationException.java
@@ -2,23 +2,8 @@ package org.dmly.traveller.app.infra.exception.flow;
 
 import org.dmly.traveller.app.infra.exception.FlowException;
 
-import javax.validation.ConstraintViolation;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 public class ValidationException extends FlowException {
-    private Set<ConstraintViolation<?>> constraints;
-
-    public ValidationException(String message, Set<ConstraintViolation<?>> constraints) {
-        super(message + constraints);
-        this.constraints = constraints;
-    }
-
     public ValidationException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public Set<ConstraintViolation<?>> getConstraints() {
-        return constraints;
     }
 }
